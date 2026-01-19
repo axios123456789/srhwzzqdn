4ed173a575d921839b69d971c7d1602b58105f24
20183 <2018304838@qq.com>
2026-01-19 09:06:16 +0800

    前端接口调用

--- srhwzzqdn-admin/src/api/sysDict.js
+++ srhwzzqdn-admin/src/api/sysDict.js
@@ -48,49 +48,57 @@ export const DeleteDictById = id => {
 export const GetAdministrative = () => {
   return request({
     url: `${base_api}/getAdministrative`,
-    method: 'get'
+    method: 'get',
   })
 }
 
 //根据type获取t_sys_code键值对（仅只有一级数据的时候）
-export const GetSysCodeByType = (type) => {
+export const GetSysCodeByType = type => {
   return request({
     url: `${base_api}/getSysCodeByType/${type}`,
-    method: 'get'
+    method: 'get',
   })
 }
 
 //获取sysCode码表的数据列表
-export const GetSysCodeLis = (queryDto2) => {
+export const GetSysCodeLis = queryDto2 => {
   return request({
     url: `${base_api}/getSysCodeList`,
     method: 'get',
-    params: queryDto2
+    params: queryDto2,
   })
 }
 
 //添加sysCode码表数据
-export const AddSysCode = (sysCode) => {
+export const AddSysCode = sysCode => {
   return request({
     url: `${base_api}/addSysCode`,
     method: 'post',
-    data: sysCode
+    data: sysCode,
   })
 }
 
 //修改sysCode码表数据
-export const UpdateSysCode = (sysCode) => {
+export const UpdateSysCode = sysCode => {
   return request({
     url: `${base_api}/updateSysCode`,
     method: 'put',
-    data: sysCode
+    data: sysCode,
   })
 }
 
 //根据id删除sysCode码表数据
-export const DeleteSysCodeById = (id) => {
+export const DeleteSysCodeById = id => {
   return request({
     url: `${base_api}/deleteSysCodeById/${id}`,
-    method: 'delete'
+    method: 'delete',
+  })
+}
+
+//获取所有sysCode码表数据
+export const GetAllSysCode = type => {
+  return request({
+    url: `${base_api}/getAllSysCodeByType/${type}`,
+    method: 'get',
   })
 }
